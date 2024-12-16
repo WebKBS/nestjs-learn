a187a35880a2952c865addbfc55eb6f0b4abc8ee
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-16 20:17:12 +0900

    feat: users 컨트롤러에서 Get Post 사용해보기

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers(): string {
+    return 'This action returns all users';
+  }
+
+  @Post()
+  createUser(): string {
+    return 'User가 생성되었습니다.';
+  }
+}
