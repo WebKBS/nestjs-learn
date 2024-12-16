385b230859acf7fc3ef58b56b8f53dde694e5f95
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-16 20:35:03 +0900

    feat: 쿼리 사용해보기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?') // GET /users/:id (optional) - 물음표를 붙이면 id가 있어도 없어도 됨
-  getUsers(@Param('id') id: string): string {
-    return id ? `User ${id} 조회되었습니다.` : 'User 전체 조회되었습니다.';
+  getUsers(@Param('id') id: string, @Query('name') name: string): string {
+    return `User ${id || ''}의 정보를 가져옵니다. ${name || ''}`;
   }
 
   @Post()
