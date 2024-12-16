32e5e68677f2952c386e81ad7b7bfe0a1fc75da3
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-16 20:25:51 +0900

    feat: 파라미터 사용해보기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  getUsers(): string {
-    return 'This action returns all users';
+  @Get('/:id?') // GET /users/:id (optional) - 물음표를 붙이면 id가 있어도 없어도 됨
+  getUsers(@Param('id') id: string): string {
+    return id ? `User ${id} 조회되었습니다.` : 'User 전체 조회되었습니다.';
   }
 
   @Post()
   createUser(): string {
-    return 'User가 생성되었습니다.';
+    return 'User 생성되었습니다.';
   }
 }
