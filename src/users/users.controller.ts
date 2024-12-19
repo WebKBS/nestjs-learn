3f7d9d0d739fea4da8145ee095638b6fe7d40805
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-19 21:23:03 +0900

    feat: pipe 기본값 추가하는 방법

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   Param,
   ParseIntPipe,
@@ -13,9 +14,11 @@ export class UsersController {
   @Get('/:id?') // GET /users/:id (optional) - 물음표를 붙이면 id가 있어도 없어도 됨
   getUsers(
     @Param('id', ParseIntPipe) id: number | undefined,
-    @Query('limit') limit: string,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number, // limit 없으면 10으로 기본값 설정
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number, // page 없으면 1로 기본값 설정
   ): string {
-    console.log(id, limit);
+    console.log(id, limit, page);
+
     return `User ${id || ''}의 정보를 가져옵니다. ${limit || ''}`;
   }
 
