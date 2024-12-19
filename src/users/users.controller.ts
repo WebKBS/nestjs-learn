87593d1ea3855c957d003549a8fc41f7d12f8797
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-19 21:18:06 +0900

    feat: pipe 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?') // GET /users/:id (optional) - 물음표를 붙이면 id가 있어도 없어도 됨
-  getUsers(@Param('id') id: string, @Query('name') name: string): string {
-    return `User ${id || ''}의 정보를 가져옵니다. ${name || ''}`;
+  getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit') limit: string,
+  ): string {
+    console.log(id, limit);
+    return `User ${id || ''}의 정보를 가져옵니다. ${limit || ''}`;
   }
 
   @Post()
