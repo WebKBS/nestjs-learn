47b8e9dce9a5ce71b0d4d2f94d835d1b418721a6
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-22 17:05:21 +0900

    feat: patch-post.dto.ts 주석 추가

--- src/posts/dto/patch-post.dto.ts
+++ src/posts/dto/patch-post.dto.ts
@@ -6,6 +6,8 @@ export class PatchPostDto extends PartialType(CreatePostDto) {
   // PartialType 을 사용하면 CreatePostDto 의 필드 중 일부만 선택적으로 사용할 수 있게 된다.
   // 이를 통해 일부 필드만 업데이트할 수 있게 된다.
   // PartialType 을 가져오려면 반드시 "@nestjs/swagger" 에서 가져와야 한다.
+  // PartialType 을 사용하면 PatchPostDto 의 문서가 모두 선택적으로 표시된다.
+  // 하지만 실제 데이터를 전달할때는 필수 값이 필터링 된다.
 
   @ApiProperty({
     description: '게시글 id',
