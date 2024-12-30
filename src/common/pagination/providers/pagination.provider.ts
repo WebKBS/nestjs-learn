2c8c4673a977218694c3a61c8c5794b126330a1c
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-30 20:35:05 +0900

    feat: 받는 요청에따라 이전페이지 및 다음 페이지 구현

--- src/common/pagination/providers/pagination.provider.ts
+++ src/common/pagination/providers/pagination.provider.ts
@@ -1,17 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PaginationQueryDto } from '../dto/pagination-query-dto';
 import { ObjectLiteral, Repository } from 'typeorm';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(
+    // Injecting request
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
   // 설명: PaginationQueryDto 를 기반으로 Repository 에서 데이터를 가져옵니다.
   public async paginatedQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
   ) {
-    return await repository.find({
+    const results = await repository.find({
       skip: (paginationQuery.page - 1) * paginationQuery.limit,
       take: paginationQuery.limit,
     });
+
+    const baseURL =
+      this.request.protocol + '://' + this.request.headers.host + '/';
+
+    const newUrl = new URL(this.request.url, baseURL);
+
+    // 계산된 페이지 수
+    const totalItems = await repository.count(); // 전체 아이템 수
+    const totalPages = Math.ceil(totalItems / paginationQuery.limit); // 전체 페이지 수
+    const nextPage =
+      paginationQuery.page === totalPages ? null : paginationQuery.page + 1;
+    // 다음 페이지
+    const prevPage =
+      paginationQuery.page === 1
+        ? paginationQuery.page
+        : paginationQuery.page - 1; // 이전 페이지
+
+    console.log(totalPages);
+
+    return results;
   }
 }
