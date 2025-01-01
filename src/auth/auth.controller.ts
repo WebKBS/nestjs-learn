3793dfe77d0bf7c0cca1a82ba5b3be1d8e513c56
kbs21c1987 <kbs21c1987@gmail.com>
2025-01-01 15:55:24 +0900

    feat: refresh 토큰 엔드포인트 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './providers/auth.service';
 import { SignInDto } from './dto/signIn.dto';
 import { AuthType } from './enums/auth-type.enum';
 import { Auth } from './decorator/auth.decorator';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -13,5 +14,12 @@ export class AuthController {
   @HttpCode(HttpStatus.OK) // 사용하는 이유는 200 OK 상태 코드를 반환하기 위함입니다.
   async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto);
+  }
+
+  @Post('refresh-token')
+  @Auth(AuthType.None) // AuthType.Refresh 에 대한 인증을 거침
+  @HttpCode(HttpStatus.OK) // 사용하는 이유는 200 OK 상태 코드를 반환하기 위함입니다.
+  async refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+    return await this.authService.refreshToken(refreshTokenDto);
   }
 }
--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,6 +2,8 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/providers/users.service';
 import { SignInDto } from '../dto/signIn.dto';
 import { SignInProvider } from './sign-in.provider';
+import { RefreshTokenDto } from '../dto/refresh-token.dto';
+import { RefreshTokenProvider } from './refresh-token.provider';
 
 @Injectable()
 export class AuthService {
@@ -9,13 +11,14 @@ export class AuthService {
     @Inject(forwardRef(() => UsersService))
     private readonly userService: UsersService,
     private readonly signInProvider: SignInProvider,
+    private readonly refreshTokenProvider: RefreshTokenProvider,
   ) {}
 
   public async signIn(signInDto: SignInDto) {
     return await this.signInProvider.signIn(signInDto);
   }
 
-  public isAuth() {
-    return true;
+  public async refreshToken(refreshTokenDto: RefreshTokenDto) {
+    return await this.refreshTokenProvider.refreshToken(refreshTokenDto);
   }
 }
