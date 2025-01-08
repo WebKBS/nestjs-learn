e8cf78fe752c992ea08290765472f1c5ca331026
kbs21c1987 <kbs21c1987@gmail.com>
2025-01-08 19:47:52 +0900

    test: app controller test 추가

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,23 @@
+import { describe } from 'node:test';
+import { AppController } from './app.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController).toBeDefined();
+    });
+  });
+});
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from './app.service'; // controller: 요청을 받아서 응답을 반환하는 역할을 한다.
 
 // controller: 요청을 받아서 응답을 반환하는 역할을 한다.
 @Controller()
